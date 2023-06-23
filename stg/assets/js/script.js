1dfa053a37ed414320063916c4660f9593ed18a4
Denver <denver@am.consulting>
2023-06-23 09:07:36 -0700

    This is the new design for the landing page

---
+++ stg/assets/js/script.js
@@ -0,0 +1,51 @@
+(function($) {
+    $(document).ready(function() {
+        $('header .header-media .mobile-menu').click(function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            $('header nav').slideToggle(350);
+        });
+
+        $('.overview-slider').slick({
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            pauseOnFocus: true
+        });
+
+        $('a[href="#back-to-top"]').click(function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            $('html, body').animate({scrollTop: 0}, 350);
+        });
+
+        $('header nav > ul > li > a').click(function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+
+            var hash = $(this).attr('href');
+            var targetElement = hash.split('#');
+
+            targetElement = $('#'+targetElement[1]);
+
+            $('html, body').animate({
+                scrollTop: targetElement.offset().top
+            }, 350);
+        });
+    });
+
+    $(window).scroll(function() {
+        var scrollTop = $(window).scrollTop();
+        
+        if(scrollTop >= 200) {
+            $('a[href="#back-to-top"]').fadeIn(350);
+            $('#overview-parallax').css('top', -(scrollTop * 0.5) + 'px');
+        } else {
+            $('a[href="#back-to-top"]').fadeOut(350);
+        }
+    });
+})(jQuery);
